439b10b95a8047a53ecac47471e768ce035a0eaa
nika chkhikvadze <124697694+BeanieTheBeanerman@users.noreply.github.com>
2023-04-06 16:57:09 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+let variable;
+console.log(variable); 
+let variable = null;
+console.log(variable); 
+let variable = 42;
+console.log(variable); 
+let pi = 3.14;
+console.log(pi); 
+
+
+
+let myComputerManufacturer = "Microsoft";
+let myComputerMemory = 16;  
+let myComputerOS = "Windows";
+let computerBuyingYear = 2012;
+let currentYear = new Date().getFullYear();
+
+
+let sentence = `My computer is ${myComputerManufacturer}, it runs on ${myComputerOS}, and I've been using it for ${yearsOfUse} years.`;
+
